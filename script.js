3db49db9aea4ca9a0bd5257ba985f08e9917d960
TaimourShmait <taimourshmaits@gmail.com>
2025-03-17 09:53:11 +0200

    Added more functionalites and the clear button

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
 const addItemButton = document.querySelector("#add-item-button");
+const clearItemsButton = document.querySelector("#clear-items-button");
 const listContainer = document.querySelector("#list-container");
 let userInputField = document.querySelector("#user-input");
 
-addItemButton.addEventListener("click", () => {
+addItemButton.addEventListener("click", addItem);
+
+userInputField.addEventListener("keypress", function(e) {
+    if (e.key === "Enter")
+        addItem();
+});
+
+clearItemsButton.addEventListener("click", clearItems);
+
+// Functions defined below
+
+function addItem () {
     let userInput = userInputField.value;
 
-    if (userInput.trim() == "") {
+    if (userInput.trim() == "") {  
         return;
     }
-    
+            
     let itemDiv = document.createElement("div");
     itemDiv.classList.add("item-container");
-    
+        
     let item = document.createElement("p");
     item.textContent = userInput;
 
     let removeItemButton = document.createElement("button");
-    removeItemButton.textContent = "Remove Item";
+    removeItemButton.id = "remove-item-button";
+    removeItemButton.textContent = "Remove";
 
     itemDiv.appendChild(item);
     itemDiv.appendChild(removeItemButton);
-
+    
     listContainer.appendChild(itemDiv);
 
     removeItemButton.addEventListener("click", () => {
@@ -29,5 +42,12 @@ addItemButton.addEventListener("click", () => {
 
     userInputField.value = "";
 
-});
+}
 
+function clearItems() {
+    let items = document.querySelectorAll(".item-container");
+    items.forEach((item) => {
+        item.remove();
+    });
+    
+}
