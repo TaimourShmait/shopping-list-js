7320932e3a16b051e2f9a8a9c305620d24b5403f
TaimourShmait <taimourshmaits@gmail.com>
2025-03-19 10:55:19 +0200

    Added edit feature

--- script.js
+++ script.js
@@ -26,15 +26,22 @@ function addItem () {
         
     let item = document.createElement("p");
     item.textContent = userInput;
+    
+    let editItemButton = document.createElement("button");
+    editItemButton.id = "edit-item-button";
+    editItemButton.textContent = "Edit";
 
     let removeItemButton = document.createElement("button");
     removeItemButton.id = "remove-item-button";
     removeItemButton.textContent = "Remove";
 
     itemDiv.appendChild(item);
+    itemDiv.appendChild(editItemButton);
     itemDiv.appendChild(removeItemButton);
     
     listContainer.appendChild(itemDiv);
+
+    editItemButton.addEventListener("click", () => editItem(item, editItemButton));
 
     removeItemButton.addEventListener("click", () => {
         itemDiv.remove();
@@ -50,4 +57,40 @@ function clearItems() {
         item.remove();
     });
     
+}
+
+function editItem(item, editItemButton) {
+    
+    editItemButton.remove();
+
+    let editItemField = document.createElement("input");
+    editItemField.setAttribute("type", "text");
+    editItemField.setAttribute("placeholder", `Edit ${item.textContent}`);
+    editItemField.id = "edit-item-field";
+
+    let confirmButton = document.createElement("button");
+    confirmButton.id = "confirm-button";
+    confirmButton.textContent = "Confirm";
+
+    // editItemField.style.fontSize = "1rem";
+    // editItemField.style.margin = "0.5em";
+    // editItemField.style.marginLeft = "-0.2em";
+
+    editItemField.style.cssText = "margin: 0.95rem 0 0.95rem 0rem ; font-size: 1rem; border: 0px solid; padding: 0.5px;";
+    item.parentNode.replaceChild(editItemField, item);
+
+    // editItemField.addEventListener("keydown", addEditedItem);
+    // editItemField.addEventListener("click", addEditedItem);
+
+    if ((editItemField.value).trim() == "") 
+        return;
+
+
+    editItemField.addEventListener("keydown", function(e) {
+        if (e.key === "Enter") {
+            item.value = editItemField.value;
+            editItemField.parentNode.replaceChild(item, editItemField);
+        }
+    });
+
 }
